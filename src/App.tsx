a2608df84fcb6b7c7ae1413d42e4e5a91859c550
JesserHamdaoui <107654958+JesserHamdaoui@users.noreply.github.com>
2024-07-06 23:28:54 +0100

    chore: manage the cofiguration and the structure of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import { Route, Routes } from "react-router-dom";
-
-import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
-import PricingPage from "@/pages/pricing";
-import BlogPage from "@/pages/blog";
-import AboutPage from "@/pages/about";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
-    </Routes>
+    <div>
+      <Home />
+    </div>
   );
 }
 
